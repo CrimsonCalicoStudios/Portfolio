e902ca49b197b9ae0147c876eb9af01b6ad5de65
Cali Aisling <83644237+CrimsonCalico@users.noreply.github.com>
2024-05-17 15:17:01 +0100

    Added controls back to 360 and fixed bottom logo on DeltaStar

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -219,7 +219,7 @@ function renderProjects(array) {
 		`;
 		} else if(project.vid) {
 			projectDiv.innerHTML = `
-			<video muted autoplay loop style="width: 90%;">
+			<video controls muted autoplay loop style="width: 90%;">
 				<source src="${project.vid}" type="video/mp4">
 				Your browser does not support the video tag.
 			</video>
